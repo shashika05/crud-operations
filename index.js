daa1a996b9797541d6e37893cb41842bb8cf4935
shashika05 <shashikaraveenyasas@gmail.com>
2023-03-23 21:27:40 +0530

    MongoDB connected

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
 const router = require("./router");
 
 const PORT = 8080;
 
 const app = express();
 
+dotenv.config();
+
 app.listen(PORT, async () => {
   console.log(`Server up on port http://localhost:${PORT}`);
 });
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(router);
